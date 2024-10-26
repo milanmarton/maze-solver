f940bd2918e3d4a9b7bfd5aa6ca44f5d8f74dc78
milanmarton <martonmilan@proton.me>
2024-10-26 14:31:54 +0200

    created point, line classes

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from maze_solver.gui import Window
+import maze_solver.gui as gui
 
 
 def main():
-    win = Window(800, 600)
+    win = gui.Window(800, 600)
+    p1 = gui.Point(100, 100)
+    p2 = gui.Point(500, 500)
+    line = gui.Line(p1, p2)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 
--- maze_solver/gui.py
+++ maze_solver/gui.py
@@ -1,16 +1,31 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        # x=0 -> left of the screen
+        # y=0 -> top of the screen
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, P1: Point, P2: Point) -> None:
+        self.P1 = P1
+        self.P2 = P2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.P1.x, self.P1.y, self.P2.x, self.P2.y, fill=fill_color, width=2
+        )
+
+
 class Window:
     def __init__(self, width: int, height: int) -> None:
-        self.width = width
-        self.height = height
         self.root = Tk()
         self.root.title("maze solver")
-        self.canvas = Canvas(
-            self.root, width=self.width, height=self.height, bg="white"
-        )
-        self.canvas.pack(fill="both", expand=True)
+        self.canvas = Canvas(self.root, width=width, height=height, bg="white")
+        self.canvas.pack(fill=BOTH, expand=True)
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -25,3 +40,6 @@ class Window:
 
     def close(self) -> None:
         self.running = False
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.canvas, fill_color)
